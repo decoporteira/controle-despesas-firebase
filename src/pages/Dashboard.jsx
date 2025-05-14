48773d0064afe7b723af91b8af459cf781e4ee7f
André Pereira <andreporteira@gmail.com>
2025-05-14 10:36:31 -0300

    Arrumando layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../services/firebase";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
@@ -17,11 +15,13 @@ export default function Dashboard() {
   };
 
   return (
-    <section>
-      <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
-      <div>
-        <button onClick={handleLogout}>Logout</button>
+    <div className="flex flex-col items-center pt-10 h-screen bg-gray-100">
+      <div className="bg-white p-6 rounded shadow-md w-200">
+        <h1 className="text-3xl font-bold mb-4">Dashboard</h1>
+        <div>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
       </div>
-    </section>
+    </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -16,6 +16,7 @@ export default function Login() {
         // Signed in
         const user = userCredential.user;
         console.log(user);
+
         navigate("/");
         // ...
       })

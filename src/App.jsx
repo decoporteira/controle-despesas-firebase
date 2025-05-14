6978a3d233e885624519f94a8510d054f24bfda9
André Pereira <andreporteira@gmail.com>
2025-05-14 11:04:39 -0300

    Simplificando login e proteção de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import { Routes, Route } from "react-router-dom";
-import PrivateRoute from "./routes/PrivateRoute.jsx";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./services/firebase.js";
 import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) return <p>Carregando...</p>;
   return (
     <>
       <Navbar />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <Dashboard />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/" element={!user ? <Login /> : <Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 import App from "./App.jsx";
-import { AuthProvider } from "./contexts/AuthContext.jsx";
+
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </BrowserRouter>
 );
